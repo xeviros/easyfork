452cce18c00afb97382c6bfc4f774d1e5996225c
Xavier Ros <x.ros.martinez@gmail.com>
2018-06-18 20:28:08 +0200

    add sku to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_18_162610) do
+ActiveRecord::Schema.define(version: 2018_06_18_182457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2018_06_18_162610) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+    t.string "sku"
     t.index ["restaurant_id"], name: "index_items_on_restaurant_id"
   end
 
