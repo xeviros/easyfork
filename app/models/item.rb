3468fc8100064366a2c91bd8659df85ae2df4d35
Xavier Ros <x.ros.martinez@gmail.com>
2018-06-19 17:02:35 +0200

    validations done

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :restaurant
+  has_many :order_items
+  has_many :orders, through: :order_items
+
   monetize :price_cents
+
+  validates :name, presence: true
+  validates :price, presence: true
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :bill
   belongs_to :user
+  has_many :order_item
+  has_many :items, through: :order_items
 end
