687ea5b5ce5e0d3d99e7e785cbe365dc5b44099a
Luca de Montis <luca.demo@hotmail.it>
2018-06-19 17:37:59 +0200

    basic structure of index pages without returning anything

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,11 +1,15 @@
 class RestaurantsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
+  def new
+    @Restaurant = Restaurant.new
+  end
+
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
-  end
-
-  def new
   end
 
   def edit
