bde7f9ef2fdb3049848ccbd90de4ca36ee381c6a
Benjamin DE BOISSIEU <bendeb@hotmail.com>
2018-06-19 12:19:45 +0200

    methos restaurants and items:

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,33 @@
 class ItemsController < ApplicationController
+
+  before_action :set_restaurant
   def new
+    @item = Item.new
   end
+
+  def create
+    @item = Item.new(item_params)
+    @item.restaurant = @restaurant
+    if @item.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
 
   def index
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :picture, :type, :restaurant_id)
+  end
+
+  def set_restaurant
+     @restaurant = Restaurant.find(params[:restaurant_id])
+  end
 end
+
+
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,7 @@
 class RestaurantsController < ApplicationController
+
+  before_action :find_rest, only: [:show, :edit, :update]
+
   def index
   end
 
@@ -10,4 +13,27 @@ class RestaurantsController < ApplicationController
 
   def edit
   end
+
+  def update
+    @restaurant.update(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def find_rest
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :description, :address, :category, :photo, :latitude, :longitude, :user_id )
+  end
+
+
+
+
 end
