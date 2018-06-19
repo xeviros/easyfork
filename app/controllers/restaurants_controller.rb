15befbb4855b3c271abfc0b070fbcbc0c66c6483
Anna Bendahan <an.bendahan@gmail.com>
2018-06-19 17:03:55 +0200

    improved

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -9,6 +9,29 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new
   end
 
+
+   def create
+      @restaurant = Restaurant.new(restaurant_params)
+      @restaurant.user = current_user
+      # calls for the same method within restaurant_policy
+      # authorize @restaurant
+      if @restaurant.save
+        redirect_to restaurant_path(@restaurant)
+      else
+        render :new
+      end
+    end
+
+
   def edit
   end
+
+
+private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :user_id, :photo, :address, :category, :description)
+  end
+
+
 end
