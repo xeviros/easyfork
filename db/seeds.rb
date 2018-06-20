d557d384916faf81115119743eb8a5f620fa376f
Luca de Montis <luca.demo@hotmail.it>
2018-06-20 11:45:56 +0200

    udpated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,9 +13,9 @@ anna = User.create(email: "anna@anna.com", password: "123456")
 xavi = User.create(email: "xavi@xavi.com", password: "123456")
 ben = User.create(email: "ben@ben.com", password: "123456")
 
-Restaurant.create(name: "Bella Italia", category: "Italian", user_id: luca, address: "Via Roma 12")
-Restaurant.create(name: "Xavi's place", category: "Spanish", user_id: xavi, address: "Carrer d'en Grassot, 101")
-Restaurant.create(name: "Vive la France", category: "French", user_id: anna, address: "51 Rue Centrale")
+Restaurant.create!(name: "Bella Italia", category: "Italian", user: luca, address: "Via Roma 12")
+Restaurant.create!(name: "Xavi's place", category: "Spanish", user: xavi, address: "Carrer d'en Grassot, 101")
+Restaurant.create!(name: "Vive la France", category: "French", user: anna, address: "51 Rue Centrale")
 
 puts "Finished!"
 
