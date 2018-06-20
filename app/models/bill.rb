7394451fc229287d5c820422989c14e70e91fc98
Luca de Montis <luca.demo@hotmail.it>
2018-06-20 17:25:33 +0200

    Crazy shit with the bill order and order item stuff happened here

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,8 +1,9 @@
 class Bill < ApplicationRecord
   belongs_to :restaurant
   has_many :orders
+  has_many :users, through: :orders
 
-  validates :number_of_people, presence: true
+  # validates :number_of_people, presence: true
   # validate :dates
 
 
@@ -11,4 +12,8 @@ class Bill < ApplicationRecord
   #     errors.add(:date, "can't be in the past")
   #   end
   # end
+
+  def has_ordered?(user)
+    self.orders.find_by(user_id: user.id)
+  end
 end
