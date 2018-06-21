a81eb7e34fef4ec78f5a29afe1c04dbc216b1fb3
lucademontis <37769008+lucademontis@users.noreply.github.com>
2018-06-21 17:19:51 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,20 +17,20 @@ resto1 = Restaurant.create!(name: "Bella Italia", category: "Italian", user: luc
 resto2 = Restaurant.create!(name: "Xavi's place", category: "Spanish", user: xavi)
 resto3 = Restaurant.create!(name: "Vive la France", category: "French", user: anna)
 
-Item.create!(category: "main", name: "Pizza", description: "Pizza woodden cooked", price: 9, restaurant: resto1)
-Item.create!(category: "main", name: "Pasta", description: "Pasta with cream and tomatos", price: 12, restaurant: resto2)
-Item.create!(category: "main", name: "chicken", price: 15, restaurant: resto3)
-Item.create!(category: "main", name: "steak", price: 10, restaurant: resto1)
+Item.create!(category: "mains", name: "Pizza", description: "Pizza woodden cooked", price: 9, restaurant: resto1)
+Item.create!(category: "mains", name: "Pasta", description: "Pasta with cream and tomatos", price: 12, restaurant: resto2)
+Item.create!(category: "mains", name: "chicken", price: 15, restaurant: resto3)
+Item.create!(category: "mains", name: "steak", price: 10, restaurant: resto1)
 
-Item.create!(category: "drink", name: "juice", price: 10, restaurant: resto1)
-Item.create!(category: "drink", name: "cappiccino", price: 10, restaurant: resto2)
-Item.create!(category: "drink", name: "avocado", price: 10, restaurant: resto3)
-Item.create!(category: "drink", name: "vegan mojito", price: 10, restaurant: resto2)
+Item.create!(category: "drinks", name: "juice", price: 10, restaurant: resto1)
+Item.create!(category: "drinks", name: "cappiccino", price: 10, restaurant: resto2)
+Item.create!(category: "drinks", name: "avocado", price: 10, restaurant: resto3)
+Item.create!(category: "drinks", name: "vegan mojito", price: 10, restaurant: resto2)
 
-Item.create!(category: "dessert", name: "Chocolat cake", price: 13, restaurant: resto1)
-Item.create!(category: "dessert", name: "Creme brule", price: 10, restaurant: resto2)
-Item.create!(category: "dessert", name: "Icecrean", description: "lemon, mint, vanilla", price: 10, restaurant: resto3)
-Item.create!(category: "dessert", name: "Fruits", price: 10, restaurant: resto3)
+Item.create!(category: "desserts", name: "Chocolat cake", price: 13, restaurant: resto1)
+Item.create!(category: "desserts", name: "Creme brule", price: 10, restaurant: resto2)
+Item.create!(category: "desserts", name: "Icecrean", description: "lemon, mint, vanilla", price: 10, restaurant: resto3)
+Item.create!(category: "desserts", name: "Fruits", price: 10, restaurant: resto3)
 
 puts "Finished!"
 
