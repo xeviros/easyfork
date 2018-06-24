4de81c1aa38f3e3da5e8968d3bcee56dd37b3f19
Xavier Ros <x.ros.martinez@gmail.com>
2018-06-24 21:45:13 +0200

    favicon and homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,8 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!
 
   def home
+    @restaurants = Restaurant.all.where.not(latitude: nil, longitude: nil).order(popularity: :desc).first(3)
   end
 
 end
