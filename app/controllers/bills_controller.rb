1fe9d261803e7f2c9836ea574d2c39dd8401fcfd
Anna Bendahan <an.bendahan@gmail.com>
2018-06-25 13:19:56 +0200

    done

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -38,7 +38,21 @@ class BillsController < ApplicationController
     # end
   end
 
+ def update
+    restaurant = Restaurant.find(params[:restaurant_id])
+    @bill.update(bill_params)
+    authorize @bill
+    if @bill.save
+      redirect_to my_orders_requests_path
+    else
+      render :edit
+    end
+  end
 
+
+ def bill_params
+    params.require(:booking).permit(:number_of_people, :total_price, :date, :restaurant_id, :user_id, :status)
+  end
   # def update
   #   @bill = Bill.find(params[:id])
   # end
