368b7402662522dc49ab09f21d0dc6d75cb127a6
Anna Bendahan <an.bendahan@gmail.com>
2018-06-27 14:05:56 +0200

    done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,24 @@
+class ReviewsController < ApplicationController
+  skip_after_action :verify_authorized
+  def new
+    @order= Order.find(params[:order_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    order = Order.find(params[:order_id])
+    # we need `restaurant_id` to asssociate review with corresponding restaurant
+    @review.order = order
+    @review.restaurant = order.bill.restaurant
+    @review.user = current_user
+    @review.save
+
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
