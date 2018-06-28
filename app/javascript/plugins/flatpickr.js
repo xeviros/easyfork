f3a7e40bf5679b5705ba04833baf9aedcf9a2559
Luca de Montis <luca.demo@hotmail.it>
2018-06-28 16:12:31 +0200

    finished date

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,12 @@
 import flatpickr from "flatpickr"
 import "flatpickr/dist/flatpickr.min.css" // Note this is important!
 
-flatpickr(".datepicker", {
-  defaultDate: new Date(),
-  enableTime: true,
-})
+
+const picker = document.querySelector(".datepicker");
+// it selects with the selector
+if (picker) {
+  flatpickr(".datepicker", {
+    defaultDate: Date.parse(picker.value),
+    enableTime: true,
+  })
+}
