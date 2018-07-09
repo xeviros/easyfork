e51a305f2cfb3fdfa1e79c0ab1979abc7343675f
Xavier Ros <x.ros.martinez@gmail.com>
2018-07-09 17:12:22 +0200

    only shown items of a particular restaurant

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -14,7 +14,7 @@ class BillsController < ApplicationController
     end
     if params[:query].present?
       sql_query = "category ILIKE :query"
-      @bill.restaurant.items = Item.where(sql_query, query: "%#{params[:query]}%").order(created_at: :desc)
+      @bill.restaurant.items.where(sql_query, query: "%#{params[:query]}%").order(created_at: :desc)
     else
       @bill.restaurant.items = @bill.restaurant.items
     end
